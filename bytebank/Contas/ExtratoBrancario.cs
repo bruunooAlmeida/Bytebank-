17a3e7a46bdb9242ea97659a979a7f65dfbf743d
bruunooAlmeida <almeidabrunoti@gmail.com>
2023-04-20 20:30:32 -0300

    Refatoração do Projeto

--- bytebank/Contas/ExtratoBrancario.cs
+++ bytebank/Contas/ExtratoBrancario.cs
@@ -1,4 +1,4 @@
-﻿using bytebank.Auxiliar;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,23 +1,16 @@
-﻿using bytebank.Auxiliar;
-using bytebank.Contas;
+﻿using bytebank.Contas;
 using bytebank.Titular;
 
-
-
 try
-{
-    
-    ContaCorrente conta1 = new ContaCorrente(283, "1234-X");
-    
+{   
+    ContaCorrente conta1 = new ContaCorrente(283, "1234-X");   
     ContaCorrente conta2 = new ContaCorrente(284, "1234-Y");
-    
     ContaCorrente conta3 = new ContaCorrente(285, "1111-x");
 
     conta1.Sacar(50);
     conta2.Sacar(100);
 
     conta1.Depositar(2500);
-
     conta1.MostrarExtratoBrancario();
 
     //Console.WriteLine(ContaCorrente.TaxaOperacao);
