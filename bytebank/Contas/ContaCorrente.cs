68776cbd2fd9c7db4395c50879d081c2f8417c54
bruunooAlmeida <almeidabrunoti@gmail.com>
2023-04-22 03:21:12 -0300

    Criando exceções personalizadas

--- bytebank/Contas/ContaCorrente.cs
+++ bytebank/Contas/ContaCorrente.cs
@@ -12,27 +12,46 @@ namespace bytebank.Contas
     {
         public static int TotalDeContasCriadas { get; private set; }
         public static int TaxaOperacao;
-        private int numero_agencia;
+        
         
         private List<ExtratoBrancario> extrato = new List<ExtratoBrancario>();
-
-        public int Numero_agencia
-        {
-            get { return this.numero_agencia; }
-            private set {
-                    if (value > 0)
-                    {
-                        this.numero_agencia = value;
-                    }
-                }
-        }
-
+        
+        public int Numero_agencia{ get; set; }
        
         public string Conta { get; set; }
 
         private double saldo = 100;
 
         public Cliente Titular { get; set; }
+
+        public int ContadorSaquesNaoPermitidos { get; set; }
+        
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
+
+        public ContaCorrente(int numero_agencia, string numero_conta)
+        {
+            this.Numero_agencia = numero_agencia;
+            this.Conta = numero_conta;
+            if (numero_agencia <= 0)
+            {
+                throw new ArgumentException("Numero da conta nao pode ser 0", nameof(numero_agencia));
+            }
+
+            if (numero_conta == null)
+            {
+                throw new ArgumentException("Codigo da agencia nao pode ser vazio", nameof(numero_conta));
+            }
+
+            if (!numero_conta.Contains('-'))
+            {
+                throw new ArgumentException("Agencia nao possui codigo indentificado", nameof(numero_conta));
+            }
+
+            GravarOperacaoBrancaria("Iniciou a Conta", ("Saldo Atual: " + this.saldo));
+
+            TotalDeContasCriadas++;
+        }
+
 
         public void Depositar(double valor)
         {
@@ -51,29 +70,28 @@ namespace bytebank.Contas
             }
             else
             {
-                return false;
+                ContadorSaquesNaoPermitidos++;
+                throw new SaldoInsuficienteException("Saldo insuficiente para Operação");
             }
         }
 
         public bool Transferir(double valor, ContaCorrente destino)
         {
-            if (saldo < valor)
-            {
-                return false;
-            }
-            else
+            try
             {
 
-                Sacar(valor);        
-                
-                destino.Depositar(valor);
-                
-                
-                return true;
-            }
+                Sacar(valor);
+                ContadorTransferenciasNaoPermitidas++;
+            }catch(SaldoInsuficienteException e)
+            {
+                throw new OperacaoFinanceiraException("Operação não realizada.", e);
+            }                
+             destino.Depositar(valor);
+               return true;
+            
         }
 
-        public void SetSaldo(double valor)
+        private void SetSaldo(double valor)
         {
             if(valor < 0)
             {
@@ -89,41 +107,10 @@ namespace bytebank.Contas
         {
             return this.saldo;
         }
-
-        public ContaCorrente(int numero_agencia, string numero_conta)
+       
+        public void mostrarSaldoConta()
         {
-            this.Numero_agencia = numero_agencia;
-            this.Conta = numero_conta;
-            if(numero_agencia <= 0)
-            {
-                throw new ArgumentException("Numero da conta nao pode ser 0", nameof(numero_agencia));
-            }
-
-            if(numero_conta == null)
-            {
-                throw new ArgumentException("Codigo da agencia nao pode ser vazio", nameof(numero_conta));
-            }
-
-            if (! numero_conta.Contains('-'))
-            {
-                throw new ArgumentException("Agencia nao possui codigo indentificado", nameof(numero_conta));
-            }
-
-            GravarOperacaoBrancaria("Iniciou a Conta", ("Saldo Atual: " + this.saldo));
-
-            //try
-            //{
-            //    TaxaOperacao = 30 / TotalDeContasCriadas;
-
-            //}
-            //catch (DivideByZeroException) 
-            //{
-            //    Console.WriteLine("Ocorreu um erro! não é permitido uma divisão por Zero");
-            //}
-            //finally { }
-
-
-            TotalDeContasCriadas++;
+            Console.WriteLine(GetSaldo());
         }
 
         public void GravarOperacaoBrancaria(string operacao,string observacao)
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,25 +1,60 @@
 ﻿using bytebank.Contas;
 using bytebank.Titular;
+using ByteBank;
 
-try
-{   
-    ContaCorrente conta1 = new ContaCorrente(283, "1234-X");   
-    ContaCorrente conta2 = new ContaCorrente(284, "1234-Y");
-    ContaCorrente conta3 = new ContaCorrente(285, "1111-x");
+LeitorDeArquivo leitor =new LeitorDeArquivo("contas.txt");
+leitor.LerProximaLinha();
+leitor.LerProximaLinha();
+leitor.Fechar();
 
-    conta1.Sacar(50);
-    conta2.Sacar(100);
+//try
+//{   
+//    ContaCorrente conta1 = new ContaCorrente(283, "1234-X");   
+//    ContaCorrente conta2 = new ContaCorrente(284, "1234-Y");
+//    ContaCorrente conta3 = new ContaCorrente(285, "1111-x");
 
-    conta1.Depositar(2500);
-    conta1.MostrarExtratoBrancario();
+//    conta1.Sacar(50);
+//    conta2.Depositar(80);
+//    //conta1.mostrarSaldoConta();
+//    //conta1.Sacar(110);
+//    conta1.MostrarExtratoBrancario();
+//    //conta2.Sacar(100);
 
-    //Console.WriteLine(ContaCorrente.TaxaOperacao);
-}
-catch(ArgumentException e) 
-{
-    Console.WriteLine("Parametro com Erro " + e.ParamName);    
-    Console.WriteLine(e.Message);
-}
+//    //    conta1.Depositar(2500);
+//    conta1.Sacar(10000);
+//    //Console.WriteLine(ContaCorrente.TaxaOperacao);
+//}
+//catch(ArgumentException e) 
+//{
+//    Console.WriteLine("Parametro com Erro " + e.ParamName);    
+//    Console.WriteLine(e.Message);
+//}
+//catch (OperacaoFinanceiraException e)
+//{
+//    Console.WriteLine(e.Message);
+//    Console.WriteLine(e.StackTrace);
+//    Console.WriteLine("Operação não pode ser concluida. Saldo Insuficente");
+//    Console.WriteLine(e.Message);
+//}
 
 
+
+//try
+//{
+//    ContaCorrente conta1 = new ContaCorrente(4564, "789684-x");
+//    ContaCorrente conta2 = new ContaCorrente(7891, "456794-y");
+
+//    // conta1.Transferir(10000, conta2);
+//    conta1.Sacar(10000);
+//}
+//catch (OperacaoFinanceiraException e)
+//{
+//    Console.WriteLine(e.Message);
+//    Console.WriteLine(e.StackTrace);
+
+//    Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
+
+//    Console.WriteLine(e.InnerException.Message);
+//    Console.WriteLine(e.InnerException.StackTrace);
+//}
 
